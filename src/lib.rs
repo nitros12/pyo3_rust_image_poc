7f3d572ad389cc7a7cd1739bca7a29416acf44de
Ben Simms <ben@bensimms.moe>
2018-08-23 02:52:28 +0100

    Add docs and func definitions

--- src/lib.rs
+++ src/lib.rs
@@ -23,6 +23,10 @@ struct ImageWrapper {
 #[pymethods]
 impl ImageWrapper {
     #[classmethod]
+    /// from_path(cls, path: str, /) -> ImageWrapper
+    /// --
+    ///
+    /// Construct an instance of `ImageWrapper` from a path.
     fn from_path(cls: &PyType, path: &str) -> PyResult<Py<ImageWrapper>> {
         let py = cls.py();
 
@@ -34,6 +38,10 @@ impl ImageWrapper {
     }
 
     #[classmethod]
+    /// from_bytes(cls, data: ByteString, /) -> ImageWrapper
+    /// --
+    ///
+    /// Construct an instance of `ImageWrapper` from a bytes like object.
     fn from_bytes(cls: &PyType, data: Vec<u8>) -> PyResult<Py<ImageWrapper>> {
         let py = cls.py();
 
@@ -46,6 +54,10 @@ impl ImageWrapper {
 
     // sadly we can't impl these with a macro because of the procedural pymethods macro
 
+    /// rotate90(self, /) -> ImageWrapper
+    /// --
+    ///
+    /// Rotate this image 90 degrees clockwise.
     fn rotate90(&self) -> PyResult<Py<ImageWrapper>> {
         let gil = Python::acquire_gil();
         let py = gil.python();
@@ -55,6 +67,10 @@ impl ImageWrapper {
         })
     }
 
+    /// rotate180(self, /) -> ImageWrapper
+    /// --
+    ///
+    /// Rotate this image 180 degrees clockwise.
     fn rotate180(&self) -> PyResult<Py<ImageWrapper>> {
         let gil = Python::acquire_gil();
         let py = gil.python();
@@ -64,6 +80,10 @@ impl ImageWrapper {
         })
     }
 
+    /// rotate270(self, /) -> ImageWrapper
+    /// --
+    ///
+    /// Rotate this image 270 degrees clockwise.
     fn rotate270(&self) -> PyResult<Py<ImageWrapper>> {
         let gil = Python::acquire_gil();
         let py = gil.python();
@@ -73,6 +93,10 @@ impl ImageWrapper {
         })
     }
 
+    /// fliph(self, /) -> ImageWrapper
+    /// --
+    ///
+    /// Rotate this image 90 degrees clockwise.
     fn fliph(&self) -> PyResult<Py<ImageWrapper>> {
         let gil = Python::acquire_gil();
         let py = gil.python();
@@ -82,6 +106,10 @@ impl ImageWrapper {
         })
     }
 
+    /// flipv(self, /) -> ImageWrapper
+    /// --
+    ///
+    /// Flip this image vertically
     fn flipv(&self) -> PyResult<Py<ImageWrapper>> {
         let gil = Python::acquire_gil();
         let py = gil.python();
@@ -91,6 +119,10 @@ impl ImageWrapper {
         })
     }
 
+    /// raw_pixels(self, /) -> bytearray
+    /// --
+    ///
+    /// Return this image's pixels as a byte array
     fn raw_pixels(&self) -> PyResult<Py<PyByteArray>> {
         let gil = Python::acquire_gil();
         let py = gil.python();
@@ -100,6 +132,10 @@ impl ImageWrapper {
         Ok(PyByteArray::new(py, &pixels).into())
     }
 
+    /// grayscale(self, /) -> ImageWrapper
+    /// --
+    ///
+    /// Return a grayscale version of this image.
     fn grayscale(&self) -> PyResult<Py<ImageWrapper>> {
         let gil = Python::acquire_gil();
         let py = gil.python();
@@ -109,12 +145,25 @@ impl ImageWrapper {
         })
     }
 
+    /// invert(self, /)
+    /// --
+    ///
+    /// Invert the colors of this image, This method operates inplace (for some reason)
     fn invert(&mut self) -> PyResult<()> {
         self.inner.invert();
 
         Ok(())
     }
 
+    /// resize(self, nwidth: int, nheight: int, filter: str, /) -> ImageWrapper
+    /// --
+    ///
+    /// Resize this image using the specified filter algorithm.
+    /// Returns a new image.
+    /// The image's aspect ratio is preserved.
+    /// The image is scaled to the maximum possible size that fits within the bounds specified by `nwidth` and `nheight`.
+    ///
+    /// The parameter `filter` should be a string of any of \"Nearest\", \"Triangle\", \"CatmullRom\", \"Gaussian\", or \"Lanczos3\".
     fn resize(&self, nwidth: u32, nheight: u32, filter: &str) -> PyResult<Py<ImageWrapper>> {
         let gil = Python::acquire_gil();
         let py = gil.python();
@@ -133,6 +182,14 @@ impl ImageWrapper {
         })
     }
 
+    /// resize_exact(self, nwidth: int, nheight: int, filter: str, /) -> ImageWrapper
+    /// --
+    /// Resize this image using the specified filter algorithm.
+    /// Returns a new image.
+    /// Does not preserve aspect ratio.
+    /// `nwidth` and `nheight` are the new image's dimensions
+    ///
+    /// The parameter `filter` should be a string of any of \"Nearest\", \"Triangle\", \"CatmullRom\", \"Gaussian\", or \"Lanczos3\".
     fn resize_exact(&self, nwidth: u32, nheight: u32, filter: &str) -> PyResult<Py<ImageWrapper>> {
         let gil = Python::acquire_gil();
         let py = gil.python();
@@ -151,6 +208,15 @@ impl ImageWrapper {
         })
     }
 
+    /// resize(self, nwidth: int, nheight: int, filter: str, /) -> ImageWrapper
+    /// --
+    ///
+    /// Resize this image using the specified filter algorithm.
+    /// Returns a new image.
+    /// The image's aspect ratio is preserved.
+    /// The image is scaled to the maximum possible size that fits within the larger (relative to aspect ratio) of the bounds specified by `nwidth` and `nheight`, then cropped to fit within the other bound.
+    ///
+    /// The parameter `filter` should be a string of any of \"Nearest\", \"Triangle\", \"CatmullRom\", \"Gaussian\", or \"Lanczos3\".
     fn resize_to_fill(&self, nwidth: u32, nheight: u32, filter: &str) -> PyResult<Py<ImageWrapper>> {
         let gil = Python::acquire_gil();
         let py = gil.python();
@@ -169,6 +235,16 @@ impl ImageWrapper {
         })
     }
 
+    /// thumbnail(self, nwidth: int, nheight: int, /) -> ImageWrapper
+    /// --
+    ///
+    /// Scale this image down to fit within a specific size.
+    /// Returns a new image.
+    /// The image's aspect ratio is preserved.
+    /// The image is scaled to the maximum possible size that fits within the bounds specified by `nwidth` and `nheight`.
+    ///
+    /// This method uses a fast integer algorithm where each source pixel contributes to exactly one target pixel.
+    /// May give aliasing artifacts if new size is close to old size.
     fn thumbnail(&self, nwidth: u32, nheight: u32) -> PyResult<Py<ImageWrapper>> {
         let gil = Python::acquire_gil();
         let py = gil.python();
@@ -178,6 +254,15 @@ impl ImageWrapper {
         })
     }
 
+    /// thumbnail_exact(self, nwidth: int, nheight: int, /) -> ImageWrapper
+    /// --
+    ///
+    /// Scale this image down to a specific size.
+    /// Returns a new image.
+    /// Does not preserve aspect ratio.
+    /// `nwidth` and `nheight` are the new image's dimensions.
+    /// This method uses a fast integer algorithm where each source pixel contributes to exactly one target pixel.
+    /// May give aliasing artifacts if new size is close to old size.
     fn thumbnail_exact(&self, nwidth: u32, nheight: u32) -> PyResult<Py<ImageWrapper>> {
         let gil = Python::acquire_gil();
         let py = gil.python();
@@ -187,6 +272,11 @@ impl ImageWrapper {
         })
     }
 
+    /// blur(self, sigma: float, /) -> ImageWrapper
+    /// --
+    ///
+    /// Performs a Gaussian blur on this image.
+    /// `sigma` is a measure of how much to blur by.
     fn blur(&self, sigma: f32) -> PyResult<Py<ImageWrapper>> {
         let gil = Python::acquire_gil();
         let py = gil.python();
@@ -196,6 +286,12 @@ impl ImageWrapper {
         })
     }
 
+    /// unsharpen(self, sigma: float, threshold: int, /) -> ImageWrapper
+    /// --
+    ///
+    /// Performs an unsharpen mask on this image.
+    /// `sigma` is the amount to blur the image by.
+    /// `threshold` is a control of how much to sharpen.
     fn unsharpen(&self, sigma: f32, threshold: i32) -> PyResult<Py<ImageWrapper>> {
         let gil = Python::acquire_gil();
         let py = gil.python();
@@ -205,6 +301,10 @@ impl ImageWrapper {
         })
     }
 
+    /// filter3x3(self, kernel: List[float], /) -> ImageWrapper
+    /// --
+    ///
+    /// Filters this image with the specified 3x3 kernel.
     fn filter3x3(&self, kernel: Vec<f32>) -> PyResult<Py<ImageWrapper>> {
         let gil = Python::acquire_gil();
         let py = gil.python();
@@ -218,6 +318,12 @@ impl ImageWrapper {
         })
     }
 
+    /// adjust_contrast(self, c: float, /) -> ImageWrapper
+    /// --
+    ///
+    /// Adjust the contrast of this image.
+    /// `contrast` is the amount to adjust the contrast by.
+    /// Negative values decrease the contrast and positive values increase the contrast.
     fn adjust_contrast(&self, c: f32) -> PyResult<Py<ImageWrapper>> {
         let gil = Python::acquire_gil();
         let py = gil.python();
@@ -227,6 +333,12 @@ impl ImageWrapper {
         })
     }
 
+    /// brighten(self, value: int, /) -> ImageWrapper
+    /// --
+    ///
+    /// Brighten the pixels of this image.
+    /// `value` is the amount to brighten each pixel by.
+    /// Negative values decrease the brightness and positive values increase it.
     fn brighten(&self, value: i32) -> PyResult<Py<ImageWrapper>> {
         let gil = Python::acquire_gil();
         let py = gil.python();
@@ -236,6 +348,13 @@ impl ImageWrapper {
         })
     }
 
+    /// huerotate(self, value: int, /) -> ImageWrapper
+    /// --
+    ///
+    /// Hue rotate the supplied image.
+    /// `value` is the degrees to rotate each pixel by.
+    /// 0 and 360 do nothing, the rest rotates by the given degree value.
+    /// just like the css webkit filter hue-rotate(180)
     fn huerotate(&self, value: i32) -> PyResult<Py<ImageWrapper>> {
         let gil = Python::acquire_gil();
         let py = gil.python();
@@ -245,10 +364,21 @@ impl ImageWrapper {
         })
     }
 
+    /// save(self, path: str)
+    /// --
+    ///
+    /// Save the buffer to a file as the path specified.
+    /// The image format is derived from the file extension.
     fn save(&self, path: &str) -> PyResult<()> {
         self.inner.save(path).map_err(|e| ImageError::new(e.to_string()))
     }
 
+    /// as_bytes(self, format: Union[str, Tuple[str, int]]) -> bytearray
+    /// --
+    ///
+    /// Save the image to a bytearray object.
+    /// `format` should be either one of the strings \"PNG\", \"GIF\", \"ICO\", \"BMP\",
+    /// or a tuple `(\"JPEG\", quality)` where `quality` is the quality of the image.
     fn as_bytes(&self, format: PyObject) -> PyResult<Py<PyByteArray>> {
         let gil = Python::acquire_gil();
         let py = gil.python();
